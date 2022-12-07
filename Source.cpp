0f5dc0b2ab2310c34b105d3fa30626a69b32d73c
Kallistrian <115313701+Kallistrian@users.noreply.github.com>
2022-12-07 01:38:06 -0800

    added library GLFW

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#define GLFW_DLL // needed for window to use the GLFW library dynamically
+#include <GLFW/glfw3.h> // The GLFW library is used to access OS-specific tasks such as opening windows, reading keyboard input, rendering, etc.
+#include <iostream>
+
+
+
+int main() {
+	if (!glfwInit()) // checks to make sure GLFW initialized successfully
+	{
+		return EXIT_FAILURE;
+		// Initialization failed
+	}
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "test window", NULL, NULL);
+	if (!window)
+	{
+		return EXIT_FAILURE;
+		// Window or OpenGL context creation failed
+	}
+
+	std::cout << "Application window opened! :)";
+	std::cin.get();
+
+	glfwDestroyWindow(window);
+	return EXIT_SUCCESS;
+}
